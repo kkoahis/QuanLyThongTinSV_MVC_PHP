ea86762293993a0fa641953f427225942bea19ef
kkoahis <khoakun27@gmail.com>
2022-11-18 18:10:57 +0700

    done insert MVC

--- Controller/C_Student.php
+++ Controller/C_Student.php
@@ -9,12 +9,36 @@ class Ctrl_Student
             $modelStudent = new Model_Student();
             $student = $modelStudent->getStudentDetail($_GET['stid']);
             include_once("../View/StudentDetail.php");
-        } else {
+        }
+
+        else if (isset($_GET['mod1'])) {
+            include_once("../View/AddStudent.php");
+        } 
+
+        else if (isset($_POST['btnAddSV'])) {
+            var_dump($_POST);
+            $id = $_REQUEST['id'];
+            $name = $_REQUEST['tensv'];
+            $age = $_REQUEST['tuoi'];
+            $university = $_REQUEST['university'];
+
+            $modelStudent = new Model_Student();
+            $modelStudent->addStudent($id, $name, $age, $university);
+            // header("location: C_Student.php?mod1='1'"); truyen mod1='1' de quay ve trang them sinhvien
+            header("location: C_Student.php");
+        }
+        
+        else if(isset($_GET['mod2'])){
+            include_once("../View/EditStudent.php");
+        }
+
+        else {
             $modelStudent = new Model_Student();
             $studentList = $modelStudent->getAllStudent();
             include_once("../View/StudentList.php");
         }
     }
 }
+
 $C_Student = new Ctrl_Student();
-$C_Student->invoke();
+$C_Student->invoke();   //mục đích là để khi vào C_Student thì luôn gọi đến hàm invoke thôi
--- Model/M_Student.php
+++ Model/M_Student.php
@@ -1,5 +1,6 @@
 <?php
 include_once("E_Student.php");
+
 class Model_Student
 {
    public function __construct()
@@ -12,13 +13,14 @@ class Model_Student
       mysqli_select_db($link, 'DULIEU999');
       $rs = mysqli_query($link, "SELECT * FROM SINHVIEN");
       $i = 0;
+
       while ($row = mysqli_fetch_array($rs)) {
          $id = $row['id'];
          $name = $row['name'];
          $age = $row['age'];
          $university = $row['university'];
-         while ($i != $id)   
-         $i++;
+         while ($i != $id)
+            $i++;
          $students[$i++] = new Entity_Student($id, $name, $age, $university);
       }
       return $students;
@@ -30,4 +32,13 @@ class Model_Student
       return $allStudent[$stid];
    }
 
+   public function addStudent($id, $name, $age, $university)
+   {
+      $link = mysqli_connect('localhost', 'root', '') or die('Could not connect:' . mysqli_error($link));
+      mysqli_select_db($link, 'DULIEU999');
+
+      $querry = "INSERT INTO SINHVIEN VALUES ($id, '$name', $age, '$university')";
+      $rs = mysqli_query($link, $querry);
+      mysqli_close($link);
+   }
 }
